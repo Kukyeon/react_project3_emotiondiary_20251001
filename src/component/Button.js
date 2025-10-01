85ec7b2c55ac2e5005a0ce3a1a6fae3a07837308
Kukyeon <rnrdus2000@naver.com>
2025-10-01 12:12:26 +0900

    Button 공통 컴포넌트 추가

---
+++ src/component/Button.js
@@ -0,0 +1,15 @@
+import "./Button.css"
+
+
+//Button컴포넌트 -> props -> 버튼의 이름 text, 이벤트 핸들러 onclick
+const Button = ({text, type, onClick}) => {
+
+    const btnType = ["positive" , "negative"].includes(type) ? type : "default";
+    //positive -> button의 클래스이름->Button Button_positive 
+
+    return(
+        <button className={["Button",`Button_${btnType}`].join(" ")} onClick={onClick}>{text}</button>
+    );   
+};
+
+export default Button;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,18 @@
-import { useSearchParams } from "react-router-dom";
+
+import Button from "../component/Button";
 
 
 const Home = () => {
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    console.log(searchParams.get("memberid")); //request.getParameter("memberid") 유사
+    // const [searchParams, setSearchParams] = useSearchParams();
+    // console.log(searchParams.get("memberid")); //request.getParameter("memberid") 유사
 
     return (
         <div>
             <h2>Home 페이지 입니다.</h2>
+            <Button type={"positive"} text={"긍정버튼"} onClick={()=>{alert("positive");}}/>
+            <Button type={"negative"} text={"부정버튼"} onClick={()=>{alert("negative");}}/>
+            <Button text={"기본버튼"} onClick={()=>{alert("default");}}/>
         </div>
     );
 }
