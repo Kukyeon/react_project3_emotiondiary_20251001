327b859a548d3f6ccd5df86027e276471bf6b628
Kukyeon <rnrdus2000@naver.com>
2025-10-01 11:36:12 +0900

    리액트 라우터 예제 코드 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
-import emotion1 from "./img/emotion1.png"
+import Diary from './pages/Diary';
+import Edit from './pages/Edit';
+import Home from './pages/Home';
+import New from './pages/New';
+import { Routes, Route, Link } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <h1> 안녕하세요 !! 모든 인류 구성원 !! </h1>
-      <img src={emotion1} />
+      <div>
+        <Link to={"/"}>홈</Link> /
+        <Link to={"/new"}>일기쓰기</Link> /
+        <Link to={"/diary/"}>일기보기</Link> /
+        <Link to={"/edit"}>일기수정</Link>
+      </div>
+      <hr></hr>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/diary/:id' element={<Diary />} />
+        <Route path='/edit/:id' element={<Edit />} />
+      </Routes>  
+      
+     
     </div>
   );
 }
---
+++ src/pages/Diary.js
@@ -0,0 +1,20 @@
+import { useParams } from "react-router-dom";
+
+
+
+const Diary = () => {
+
+    const {id} = useParams(); //@PathVariable String id 와 유사
+    
+
+
+    return (
+        <div>
+            
+            <h2>Diary 페이지 입니다.</h2>
+            <h3>{id}번 일기를 보고 계십니다.</h3>
+        </div>
+    );
+}
+
+export default Diary;
